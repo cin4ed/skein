d18b4f2bedf7efd67ebd09710a20daa38c411fbd
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2025-05-06 14:08:59 -0700

    v1.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 import "./index.css";
 
 function App() {
+  const [rects, setRects] = useState<{ x: number; y: number }[]>([]);
+
+  function handleClick(e: React.MouseEvent<SVGSVGElement>) {
+    const svg = e.currentTarget;
+    const bounds = svg.getBoundingClientRect();
+    const x = e.clientX - bounds.left;
+    const y = e.clientY - bounds.top;
+    setRects((rects) => [...rects, { x, y }]);
+  }
+
   return (
     <div style={{ height: "100vh" }}>
-      <svg style={{ width: "100%", height: "100%", display: "block" }}>
-        <rect width="100" height="100" x="100" y="100" />
-        <circle cx="300" cy="150" r="50" />
-        <line x1="100" y1="250" x2="350" y2="250" stroke="black" />
+      <svg
+        style={{ width: "100%", height: "100%", display: "block" }}
+        onClick={handleClick}
+      >
+        {rects.map((rect) => (
+          <rect x={rect.x} y={rect.y} width="100" height="100" />
+        ))}
       </svg>
     </div>
   );
